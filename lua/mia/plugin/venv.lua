9286a40774e3e30e55dc38a04fd0e7a38ecb7be3
Emilia Simmons <emilia.milisims@gmail.com>
2025-03-03 20:48:14 -0500

    fix: use data path for venv

--- lua/mia/plugin/venv.lua
+++ lua/mia/plugin/venv.lua
@@ -1,9 +1,9 @@
-local cfg = vim.fn.stdpath('config') --[[@as string]]
+local datapath = vim.fn.stdpath('data') --[[@as string]]
 
 local M = {
-  path = vim.fs.joinpath(cfg, '.venv'),
-  bin = vim.fs.joinpath(cfg, '.venv/bin'),
-  prog = vim.fs.joinpath(cfg, '.venv/bin/python'),
+  path = vim.fs.joinpath(datapath, 'venv'),
+  bin = vim.fs.joinpath(datapath, 'venv/bin'),
+  prog = vim.fs.joinpath(datapath, 'venv/bin/python'),
 }
 
 vim.g.python3_host_prog = M.prog
