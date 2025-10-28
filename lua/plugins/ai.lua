b5736eefe30943b53447ae3ff099b75db805d277
Emilia Simmons <emilia.milisims@gmail.com>
2025-10-28 18:08:12 -0400

    update: simplify cc opts

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,58 +1,66 @@
-local pt = { provider = 'snacks' }
-local ot = { opts = pt }
 vim.env.GEMINI_API_KEY = mia.secrets.gemini()
+
 ---@type LazySpec
 return {
   'olimorris/codecompanion.nvim',
+  enabled = mia.ide.enabled,
   cmd = { 'CodeCompanion', 'CodeCompanionActions', 'CodeCompanionChat', 'CodeCompanionCmd' },
-  ctx = {
+  ctxmap = {
     {
       mode = 'ca',
-      ctx = 'builtin.cmd_start',
-      each = {
-        cc = 'CodeCompanion',
-        cca = 'CodeCompanionActions',
-        cch = 'CodeCompanionChat',
-        ccc = 'CodeCompanionCmd',
-      },
+      ctx = 'cmd.start',
+      { 'cc', 'CodeCompanion' },
+      { 'cca', 'CodeCompanionActions' },
+      { 'cch', 'CodeCompanionChat' },
+      { 'ccc', 'CodeCompanionCmd' },
     },
   },
   keys = { { '<C-c>', '<Plug>(cc-stop)', ft = 'codecompanion' } },
-  --   { '<C-g><C-g>', '<Plug>(cc-send)', remap = true, mode = 'i', ft = 'codecompanion' },
-  --   { '<C-g>', '<Plug>(cc-send)', remap = true, ft = 'codecompanion' },
-  --   { ']c', '<Plug>(cc-next-chat)', remap = true, ft = 'codecompanion' },
-  --   { '[c', '<Plug>(cc-prev-chat)', remap = true, ft = 'codecompanion' },
-  -- },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
+    {
+      'ravitemer/mcphub.nvim',
+      enabled = mia.ide.enabled,
+      event = 'VeryLazy',
+      build = 'npm install -g mcp-hub@latest',
+      opts = {
+        port = 3000,
+        config = vim.fn.stdpath('config') .. '/mcpservers.json',
+        shutdown_delay = 0 * 1000,
+      },
+    },
   },
   opts = {
     adapters = {
-      think = function()
-        return require('codecompanion.adapters').extend('gemini', {
-          name = 'Gemini Thinking',
-          schema = { model = { default = 'gemini-2.0-flash-thinking-exp' } },
-        })
-      end,
-      flash = function()
-        return require('codecompanion.adapters').extend('gemini', {
-          schema = { model = { default = 'gemini-2.0-flash-lite-preview' } },
-        })
-      end,
-      pro = function()
-        return require('codecompanion.adapters').extend('gemini', {
-          name = 'Gemini Pro',
-          schema = { model = { default = 'gemini-2.0-pro-exp' } },
-          -- schema = { model = { default = 'gemini-2.0-pro-exp-02-05' } },
-        })
-      end,
+      acp = {
+        gemini_cli = function()
+          return require('codecompanion.adapters').extend('gemini_cli', {
+            env = {
+              api_key = mia.secrets.gemini(),
+            },
+          })
+        end,
+      },
     },
-    -- display = { action_palette = pt },
+    display = {
+      chat = { show_settings = true },
+      action_palette = {
+        opts = {
+          show_default_actions = false,
+          show_default_prompt_library = false,
+        },
+      },
+    },
     strategies = {
       chat = {
-        slash_commands = { buffer = ot, file = ot, symbols = ot, workspace = ot },
         adapter = 'gemini',
+        slash_commands = {
+          file = { opts = { provider = 'snacks' } },
+          symbols = { opts = { provider = 'snacks' } },
+          buffer = { opts = { provider = 'snacks' } },
+          help = { opts = { provider = 'snacks' } },
+        },
 
         keymaps = {
           completion = { modes = { i = '<Plug>(disabled)' } },
@@ -61,8 +69,17 @@ return {
           next_chat = { modes = { n = ']c' } },
           previous_chat = { modes = { n = '[c' } },
           stop = { modes = { n = '<Plug>(cc-stop)' } },
-          options = { modes = { n = 'g?' } }
+          options = { modes = { n = 'g?' } },
         },
+      },
+    },
+    tools = {
+      mcp = {
+        callback = function()
+          return require('mcphub.extensions.codecompanion')
+        end,
+        description = 'Call tools and resources from the MCP Servers',
+        opts = { user_approval = true },
       },
     },
   },
