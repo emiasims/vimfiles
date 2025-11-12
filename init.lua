b88f5af0d05296c7ae07ab05682a798241e71091
Emilia Simmons <emilia.milisims@gmail.com>
2025-11-12 18:19:56 -0500

    fix: move loader.enable()

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
+vim.loader.enable()
 _G.mia = require('mia')
 
 mia.load('plugin') -- loads all in mia/plugin
-require('mia.lazy_init')  -- plugins
+require('mia.lazy_init') -- plugins
 mia.load('after')
