66978d051d8bd02fe9a1391c1f7acc09ff269493
Emilia Simmons <emilia.milisims@gmail.com>
2025-11-25 23:21:02 -0500

    add: ctxmaps for fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,37 @@
 ---@type LazySpec
 return {
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    lazy = false,
+    cmd = 'G',
+    ctxmap = {
+      {
+        ctx = 'cmd.start(lhs, map) and abbr.trigger(" ")',
+        mode = 'ca',
+        { 'gau', 'G add --update' },
+        { 'gst', 'G status' },
+        { 'gco', 'G checkout' },
+        { 'gad', 'G add' },
+        { 'gau', 'G add --update' },
+        { 'gaup', 'G add --update --patch' },
+      },
+      {
+        ctx = 'cmd.start',
+        mode = 'ca',
+        { 'gau', 'G add --update %' },
+        { 'gst', 'G status <C-r>=expand("%:h")<Cr>' },
+        { 'gpl', 'G pull' },
+        { 'gps', 'G push' },
+        { 'gcim', "G commit -m ''<Left>", eat = '%s' },
+        { 'gco', 'G checkout %' },
+        { 'gad', 'G add %' },
+        { 'gau', 'G add --update %' },
+        { 'gaup', 'G add --update --patch %' },
+        -- TODO cfg files
+        clear = false,
+      },
+    },
+  },
   {
     'kdheepak/lazygit.nvim',
     lazy = true,
