f07d1e322fb5a3a86f3ff02acde9d8d412191ac5
Emilia Simmons <emilia.milisims@gmail.com>
2025-12-11 16:03:06 -0500

    fix: slow help loading - disable highlight colors

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -2,5 +2,8 @@
 return { -- Highlight color codes like #ca9ee6, rgb(202,158,230), etc.
   'brenoprata10/nvim-highlight-colors',
   event = 'VeryLazy',
-  opts = { enable_named_colors = false },
+  opts = {
+    enable_named_colors = false,
+    exclude_filetypes = { 'lazy', 'help' }
+  },
 }
