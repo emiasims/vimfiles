390bcb4722ec86fed810ea9c06d58fcbd353942e
Emilia Simmons <emilia.milisims@gmail.com>
2025-12-11 19:08:58 -0500

    refactor: conform to own file

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+---@type LazySpec
+return {
+  'stevearc/conform.nvim',
+  dependencies = { 'mason.nvim' },
+  opts = {
+    formatters_by_ft = {
+      lua = { 'stylua', lsp_format = 'never' },
+      python = { 'isort', 'black' },
+      markdown = { 'prettier', 'inject' },
+    },
+  },
+  ---@param c {opts: conform.setupOpts}
+  config = function(c)
+    require('conform').setup(c.opts)
+    mia.fmtexpr = mia.restore_opt( --
+      { eventignore = 'all' },
+      function()
+        require('conform').formatexpr()
+        vim.schedule_wrap(vim.cmd.doautocmd)('TextChanged')
+      end
+    )
+
+    vim.o.formatexpr = 'v:lua.mia.fmtexpr()'
+  end,
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,31 +8,6 @@ return {
   dependencies = {
     'jose-elias-alvarez/null-ls.nvim',
     'williamboman/mason-lspconfig.nvim',
-
-    {
-      'stevearc/conform.nvim',
-      opts = {
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          python = { 'isort', 'black' },
-          markdown = { 'prettier', 'inject' },
-        },
-      },
-      ---@param c {opts: conform.setupOpts}
-      config = function(c)
-        require('conform').setup(c.opts)
-        mia.fmtexpr = mia.restore_opt( --
-          { eventignore = 'all' },
-          function()
-            require('conform').formatexpr()
-            vim.schedule_wrap(vim.cmd.doautocmd)('TextChanged')
-          end
-        )
-
-        vim.o.formatexpr = 'v:lua.mia.fmtexpr()'
-      end,
-    },
-
     {
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       opts = {
