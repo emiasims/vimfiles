9de7cd8a9204eae050309340ffaf8b915c5406cd
Emilia Simmons <emilia.milisims@gmail.com>
2025-12-24 18:19:35 -0500

    refactor: use ts-textobjects for dsf

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -19,29 +19,53 @@ return {
   config = function(_, opts)
     require('nvim-treesitter-textobjects').setup(opts)
 
-    local function ts_map(mode, lhs, fn, query, group, km_opts)
-      vim.keymap.set(mode, lhs, function()
-        return fn(query, group or 'textobjects')
-      end, km_opts)
-    end
-
     local swap = require('nvim-treesitter-textobjects.swap')
-    ts_map('n', '<leader>a', swap.swap_next, '@parameter.inner')
-    ts_map('n', '<leader>A', swap.swap_previous, '@parameter.inner')
-
     local move = require('nvim-treesitter-textobjects.move')
-    ts_map({ 'n', 'o', 'x' }, ']m', move.goto_next_start, '@function.outer')
-    ts_map({ 'n', 'o', 'x' }, '[m', move.goto_previous_start, '@function.outer')
-    ts_map({ 'n', 'o', 'x' }, ']M', move.goto_next_end, '@function.outer')
-    ts_map({ 'n', 'o', 'x' }, '[M', move.goto_previous_end, '@function.outer')
-    ts_map({ 'n', 'o', 'x' }, '[[', move.goto_previous_start, '@class.outer')
-    ts_map({ 'n', 'o', 'x' }, ']]', move.goto_next_start, '@class.outer')
-    ts_map({ 'n', 'o', 'x' }, '[]', move.goto_next_end, '@class.outer')
-
     local sel = require('nvim-treesitter-textobjects.select')
-    ts_map({ 'o', 'x' }, 'af', sel.select_textobject, '@function.outer')
-    ts_map({ 'o', 'x' }, 'if', sel.select_textobject, '@function.inner')
-    ts_map({ 'o', 'x' }, 'ac', sel.select_textobject, '@class.outer')
-    ts_map({ 'o', 'x' }, 'ic', sel.select_textobject, '@class.inner')
+
+    mia.keymap({
+      { '<leader>a', function() swap.swap_next('@parameter.inner', 'textobjects') end },
+      { '<leader>A', function() swap.swap_previous('@parameter.inner', 'textobjects') end },
+    })
+
+    mia.keymap({
+      mode = { 'n', 'o', 'x' },
+      { ']m', function() move.goto_next_start('@function.outer', 'textobjects') end },
+      { '[m', function() move.goto_previous_start('@function.outer', 'textobjects') end },
+      { ']M', function() move.goto_next_end('@function.outer', 'textobjects') end },
+      { '[M', function() move.goto_previous_end('@function.outer', 'textobjects') end },
+      { '[[', function() move.goto_previous_start('@class.outer', 'textobjects') end },
+      { ']]', function() move.goto_next_start('@class.outer', 'textobjects') end },
+      { '[]', function() move.goto_next_end('@class.outer', 'textobjects') end },
+    })
+
+    mia.keymap({
+      mode = { 'o', 'x' },
+      { 'af', function() sel.select_textobject('@function.outer', 'textobjects') end },
+      { 'if', function() sel.select_textobject('@function.inner', 'textobjects') end },
+      { 'ac', function() sel.select_textobject('@class.outer', 'textobjects') end },
+      { 'ic', function() sel.select_textobject('@class.inner', 'textobjects') end },
+    })
+
+    mia.keymap({
+      'dsf',
+      desc = 'Delete surrounding function',
+      dotrepeat = true,
+      function()
+        local tsconfig = require('nvim-treesitter-textobjects.config')
+        local cfg = tsconfig.select
+        tsconfig.update({ select = { lookahead = false, lookbehind = false } })
+        pcall(function()
+          local tselect = require('nvim-treesitter-textobjects.select')
+          tselect.select_textobject('@parameter.inner', 'textobjects')
+          -- h moves on to the parens, if the arg is a function we might just
+          -- select that function instead. So that fixes it most of the time
+          vim.api.nvim_feedkeys('yh', 'nx', true)
+          tselect.select_textobject('@call.outer', 'textobjects')
+          vim.api.nvim_feedkeys('p', 'nx', true)
+        end)
+        tsconfig.update({ select = cfg })
+      end,
+    })
   end,
 }
