4b50e3a89b8caceb3e99c86914fed8ac767224b7
Emilia Simmons <emilia.milisims@gmail.com>
2025-12-30 00:43:07 -0500

    add: vim/mia hls

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -1,3 +1,10 @@
-; extends
+;; extends
 
 (string [ "\"" "'" "[[" ] @comment  [ "\"" "'" "]]" ] @comment)
+
+((identifier) @module.builtin
+  (#any-of? @module.builtin "mia" "vim"))
+
+(dot_index_expression
+  table: (identifier) @_G (#eq? @_G "_G")
+  field: (identifier) @module.builtin)
