b9a8f4149544d7bc030e55669e156e8293af4839
Emilia Simmons <emilia.milisims@gmail.com>
2026-03-16 12:37:52 -0700

    add: if name == main fold

--- queries/python/folds.scm
+++ queries/python/folds.scm
@@ -6,3 +6,5 @@
 
 (class_definition (block . (expression_statement . (string) @fold (#match? @fold "^\"\"\""))))
 (function_definition (block . (expression_statement . (string) @fold (#match? @fold "^\"\"\""))))
+
+(if_statement (comparison_operator (identifier) @_name (#eq? @_name "__name__") (string (string_content) @_main (#eq? @_main "__main__")))) @fold
